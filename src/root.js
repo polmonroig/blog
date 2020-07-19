29746d47f80ef2da13a4f1822f06044f14b5d7b9
polmonroig <polmonroig@yahoo.es>
2020-07-19 18:08:03 +0200

    Update root (test)

--- src/root.js
+++ src/root.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 
-class Root extends React.Component {
+// http request for articles 
+const url = 'https://dev.to/api/articles?username=polmonroig';
+const http = new XMLHttpRequest(); 
+http.open("GET", url);
+http.send(); 
+http.onreadystatechange=(e)=>{
+    console.log(http.responseText); 
+}
 
+class Root extends React.Component {
+    
     render(){
         return (
+            <>
+            <h1>Personal Blog</h1>
             <p>Hello world</p>
+            </>
         );
     }
 }
