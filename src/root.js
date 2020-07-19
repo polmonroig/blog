227db3b8194566876287623c557e9e41ffb8df08
polmonroig <polmonroig@yahoo.es>
2020-07-19 12:01:21 +0200

    Add node package

---
+++ src/root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class Root extends React.Component {
+
+    render(){
+        return (
+            <p>Hello world</p>
+        );
+    }
+}
+
+export default Root;
