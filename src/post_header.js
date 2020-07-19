aec279ab6ef66a3f4f9c708072e071d1515ec813
polmonroig <polmonroig@yahoo.es>
2020-07-19 20:14:12 +0200

    Add styles to title

---
+++ src/post_header.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+
+
+class PostHeader extends React.Component{
+    
+
+    render(){
+        return (
+            <>
+            <h2>{this.props.title}</h2>
+            <p>{this.props.content}</p>
+            <b>Comments </b> {this.props.comments}
+            <b> Reactions </b> {this.props.reactions}
+            </>
+        ) 
+    }
+
+
+}
+
+export default PostHeader; 
