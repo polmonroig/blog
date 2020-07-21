c503692b1a16535c86125a09ad0888d0f1bb276a
polmonroig <polmonroig@yahoo.es>
2020-07-21 14:00:47 +0200

    Add code highlight

--- src/post.js
+++ src/post.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './styles/post.css'; 
+import './styles/post.css';
 
 class Post extends React.Component{
 
 
     render(){
         return (
+            <>
+            <div id="post_meta" ></div>
             <div id="post" dangerouslySetInnerHTML={{ __html: this.props.content }}></div>
+            </>
         );
     }
 
